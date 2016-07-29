37cd5398acdd01a443ac529834a8770c915dbfc9
Margaret Ochieng <margaret.ochieng@andela.com>
2016-07-29 14:17:22 +0300

    Feature - create different setting for develop, testing and production of the app

--- bucketlist/settings/base.py
+++ bucketlist/settings/base.py
@@ -13,8 +13,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$vsx*386r@whc=t0xftrw2)xufapi5=v0r(z)b^a_pqi7bh5#)'
 
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -90,20 +90,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bucketlist.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bucketlist',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
 
 
 # Password validation
