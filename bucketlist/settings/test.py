31f53171978d15830517f3a4e5af295e18a9effd
Margaret Ochieng <margaret.ochieng@andela.com>
2016-08-05 11:09:11 +0300

    [Bug fix] setting test secret key

--- bucketlist/settings/test.py
+++ bucketlist/settings/test.py
@@ -5,7 +5,7 @@ Test specific settings.
 from .base import *
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = " Your my test secret"
 
 NOSE_ARGS = [
     '--with-coverage',
